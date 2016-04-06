e77ff1f53e15767de19185f529c3f9277c07bb7b
Dimidrol <dkonygin@gmail.com>
2016-04-06 18:21:51 +0300

    add FMTableViewCell

---
+++ FMTableView/FMTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FMTableViewCell.h
+//  FMTableView
+//
+//  Created by Dimidrol on 06.04.16.
+//  Copyright © 2016 FM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMTableViewCell : UITableViewCell
+
+@end
---
+++ FMTableView/FMTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  FMTableViewCell.m
+//  FMTableView
+//
+//  Created by Dimidrol on 06.04.16.
+//  Copyright © 2016 FM. All rights reserved.
+//
+
+#import "FMTableViewCell.h"
+
+@implementation FMTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
